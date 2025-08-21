a0309e40b0f78debf5a023d43d99c4342ab410ef
Emmanuel Oyewole <oyewoleemmanuel300@gmail.com>
2025-08-21 17:40:06 +0100

    - added paginated list comment response.

--- api/shared/comment_repo.py
+++ api/shared/comment_repo.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import desc, func, select
 from sqlalchemy.orm import selectinload
 from ..comment.model import Comment
 from ..comment.schema import CommentCreate
+from ..shared.pagination import PaginationParams, PaginatedResponse
 
 
 class CommentRepository:
@@ -21,8 +22,43 @@ class CommentRepository:
         query = select(Comment).where(Comment.id == comment_id)
 
         if load_relation:
-            query = query.options(selectinload(Comment.replies), selectinload(Comment.author))
+            query = query.options(
+                selectinload(Comment.replies), selectinload(Comment.author)
+            )
 
         result = await self.db.execute(query)
 
         return result.scalar_one_or_none()
+
+    async def list_by_blog(
+        self, blog_id: str, pagination: PaginationParams, load_relation: bool = True
+    ) -> PaginatedResponse[Comment]:
+        count_query = select(Comment).where(Comment.blog_id == blog_id)
+        total = await self.db.scalar(select(func.count()).select_from(count_query))
+
+        offset = (pagination.page - 1) * pagination.per_page
+        query = (
+            select(Comment).where(Comment.blog_id == blog_id)
+            # .order_by(Comment.created_at)
+            # .offset(offset)
+            # .limit(pagination.per_page)
+        )
+
+        if load_relation:
+            query = query.options(
+                selectinload(Comment.replies), selectinload(Comment.author)
+            )
+        query = (
+            query.order_by(desc(Comment.created_at))
+            .offset(offset)
+            .limit(pagination.per_page)
+        )
+        result = await self.db.execute(query)
+        comments = result.scalars().unique().all()
+
+        return PaginatedResponse.create(
+            items=comments,
+            total=total,
+            page=pagination.page,
+            per_page=pagination.per_page,
+        )
